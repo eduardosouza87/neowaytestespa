543892cdd901cf7c63036d1eae95649deaa5bc00
Eduardo de Souza <falecom@dnaestudiocriativo.com.br>
2024-08-02 22:46:55 -0300

    Feat: Configurado API Base URL e API KEY. Por enquanto está pública, mas será alterada mais a frente no projeto

--- app.vue
+++ app.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <!-- <div>
+    <h1>Últimas Notícias</h1>
+    <div v-if="error">{{ error.message }}</div>
+    <div v-else-if="isLoading">Carregando...</div>
+    <div v-else>
+      <div
+        v-for="article in news.articles"
+        :key="article.url"
+      >
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+      </div>
+    </div>
+  </div> -->
 </template>
+
+<script setup>
+// const runtimeConfig = useRuntimeConfig()
+// console.log(runtimeConfig.public.apiBaseUrl)
+
+// const { data: news, pending: isLoading, error } = await useFetch(
+//   `?q=technology`,
+//   {
+//     server: false,
+//     baseURL: runtimeConfig.public.apiBaseUrl,
+//     headers: {
+//       authorization: `Bearer ${runtimeConfig.public.apiKey}`
+//     }
+//   }
+// )
+
+// const { data: news, pending: isLoading, error } = await useFetch('/api/news')
+</script>
---
+++ server/api/news.ts
@@ -0,0 +1,16 @@
+// import { useFetch } from "nuxt/app";
+
+// export default defineEventHandler(async (event) => {
+//   const config = useRuntimeConfig()
+//   const { apiBaseUrl } = config.public
+//   const apiKey = config.apiKey
+
+// 	console.log(apiBaseUrl)
+
+// 	const { data, pending, error, refresh } = await useFetch(`${apiBaseUrl}?q=technology&apiKey=${apiKey}`,{
+// 	})
+
+//   // const data = await response.json()
+
+//   return data
+// });
