910a6e112f963d3d6162cda93e724b0c2a15c8be
Eduardo de Souza <falecom@dnaestudiocriativo.com.br>
2024-08-07 18:09:18 -0300

    fix: apiKey direto no composable

--- composables/useGetNews.ts
+++ composables/useGetNews.ts
@@ -1,15 +1,11 @@
 export const useGetNews = () => {
   const config = useRuntimeConfig()
   const apiKey = config.public.apiKey
-  const apiUrl = `${config.public.apiBaseUrl}?q=bitcoin`
+  const apiUrl = `${config.public.apiBaseUrl}?q=bitcoin&apiKey=8857303130a944c8a7ef9e36dca51803`
 
   const fetchNews = async () => {
     try {
-      const data = await $fetch(apiUrl, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      })
+      const data = await $fetch(apiUrl)
       return data
     } catch (error) {
       return { error: error.message }
