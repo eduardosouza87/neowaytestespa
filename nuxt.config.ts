95080ae13593a22ce72aa17fd1ac15dd665c651d
Eduardo de Souza <falecom@dnaestudiocriativo.com.br>
2024-08-07 17:42:00 -0300

    build: sem baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
         }
       ]
     },    
-    baseURL: process.env.NODE_ENV === 'production' ? '/neowaytestespa' : '/',
+    // baseURL: process.env.NODE_ENV === 'production' ? '/neowaytestespa' : '/',
   },
 
   runtimeConfig: {
