a5d6db768406a272786b48c31212405ebb3367f8
Eduardo de Souza <falecom@dnaestudiocriativo.com.br>
2024-08-07 16:54:06 -0300

    build: removido nitro e folder dist atualizado

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
-  nitro: {
-    static: true,
-    prerender: {
-      crawlLinks: true,
-      routes: [
-        '/',
-        '/404',
-        '/200'
-      ]
-    }
-  },
-
   app: {
     head: {
       charset: 'utf-8',
